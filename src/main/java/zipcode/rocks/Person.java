67e51aad4401443065937a9b1b29528e89475a29
Anthony <145401343+AnthonyP365@users.noreply.github.com>
2024-02-29 08:08:03 -0500

    I added Cohort

--- src/main/java/zipcode/rocks/Person.java
+++ src/main/java/zipcode/rocks/Person.java
@@ -3,6 +3,10 @@ package zipcode.rocks;
 public class Person {
     private String firstname;
     private String lastname;
+    private String githubHandle;
+    private String favoriteColor;
+    private String birthMonth;
+    private boolean havePets;
 
     public Person(String fname, String lname) {
         this.firstname = fname;
@@ -26,7 +30,39 @@ public class Person {
         this.lastname = lastname;
     }
 
+    public String getGitHubHandle() {
+        return githubHandle;
+    }
+
+    public void setGithubHandle(String githubHandle) {
+        this.githubHandle = githubHandle;
+    }
+
+    public String getFavoriteColor() {
+        return favoriteColor;
+    }
+
+    public void setFavoriteColor(String favoriteColor) {
+        this.favoriteColor = favoriteColor;
+    }
+
+    public String getBirthMonth() {
+        return birthMonth;
+    }
+
+    public void setBirthMonth(String birthMonth) {
+        this.birthMonth = birthMonth;
+    }
+
+    public boolean isHavePets() {
+        return havePets;
+    }
+
+    public void setHavePets(boolean havePets) {
+        this.havePets = havePets;
+    }
+
     public String toString() {
-        return this.lastname + ", " + this.firstname;
+        return this.lastname + ", " + this.firstname + ", " + this.githubHandle + ", " + this.favoriteColor + ", " + this.birthMonth + ", " + this.havePets;
     }
 }
